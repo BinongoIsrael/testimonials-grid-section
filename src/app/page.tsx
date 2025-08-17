6f0a6777db490939ed9966c0d8d1952d38959fa9
Israel <israelmelorenbinongo@gmail.com>
2025-08-18 00:17:20 +0800

    Update page to use 100vh on md screens and allow scrolling on smaller devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { testimonials } from "@/data/testimonials";
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-light-grayish-blue">
-      <section className="mx-auto mt-[12.692rem] mb-[12.692rem] max-w-[77%] grid grid-cols-4 gap-[2.308rem] justify-center md:mt-20">
+      <section className="mx-auto mt-[10rem] mb-[10rem] max-w-[77%] grid grid-cols-4 gap-[2.308rem] justify-center md:mt-20 md:mb-20">
         {testimonials.map((testimonial, index) => (
           <TestimonialCard
             key={index}
